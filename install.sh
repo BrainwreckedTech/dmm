2010249bdd3c6a7e0efdf76ecef3fbabcac088f8
BrainwreckedTech <3752022+BrainwreckedTech@users.noreply.github.com>
2022-11-16 20:47:31 -0500

    Fix permission issues.

--- install.sh
+++ install.sh
@@ -7,6 +7,8 @@ SOURCE='.'
 DESTINATION='/usr/local'
 
 set -x
-/usr/bin/sudo /usr/bin/install -Dm 755 "${SOURCE}/dmm" "${DESTINATION}/bin/dmm"
-/usr/bin/sudo /usr/bin/install -Dm 644 "${SOURCE}/dmmlogo.png" "${DESTINATION}/share/pixmaps/dmmlogo.png"
-/usr/bin/sudo /usr/bin/install -Dm 644 "${SOURCE}/dmm.desktop" "${DESTINATION}/share/applications/dmm.desktop"
+/usr/bin/sudo /usr/bin/install -D "${SOURCE}/dmm" "${DESTINATION}/bin/dmm"
+/usr/bin/sudo /usr/bin/install -D "${SOURCE}/dmmlogo.png" "${DESTINATION}/share/pixmaps/dmmlogo.png"
+/usr/bin/sudo /bin/chmod 644 "${DESTINATION}/share/pixmaps/dmmlogo.png"
+/usr/bin/sudo /usr/bin/install -D "${SOURCE}/dmm.desktop" "${DESTINATION}/share/applications/dmm.desktop"
+/usr/bin/sudo /bin/chmod 644 "${DESTINATION}/share/applications/dmm.desktop"
